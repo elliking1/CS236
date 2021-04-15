0ba49fdc901eb296f65f7557ba5760daf730f589
elliking1 <71565785+elliking1@users.noreply.github.com>
2021-04-14 20:47:21 -0600

    Update Interpreter.cpp

--- Project 4/Interpreter.cpp
+++ Project 4/Interpreter.cpp
@@ -1,11 +1,3 @@
-//
-//  Interpreter.cpp
-//  Project3
-//
-//  Created by Cam on 3/5/19.
-//  Copyright © 2019 Cam. All rights reserved.
-//
-
 #include "Interpreter.h"
 
 Interpreter :: Interpreter() {
