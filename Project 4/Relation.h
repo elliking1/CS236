b19a9a8b4322f4c0d48dfaaa327689e52ae7e0d9
elliking1 <71565785+elliking1@users.noreply.github.com>
2021-04-14 20:56:01 -0600

    Update Relation.h

--- Project 4/Relation.h
+++ Project 4/Relation.h
@@ -1,23 +1,46 @@
-using namespace std;
+#ifndef Relation_h
+#define Relation_h
 
-#ifndef RELATION_H
-#define RELATION_H
-
-#include <iostream>
 #include <stdio.h>
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <set>
-#include <vector>
+#include <map>
+#include "Tuple.h"
+#include "Scheme.h"
+
+
+using namespace std;
 
 class Relation {
 public:
-	Relation(string, vector<string>);
-	~Relation();
-	void addRow(vector<string>);
-	string toString();
-	string name;
-	vector<string> scheme;
-	set<vector<string>> rows;
+    Relation();
+    Relation(string n, Scheme s);
+    Relation(string n, set<Tuple> s);
+    Relation(string n);
+    ~Relation();
+    
+    string toString();
+    
+    bool addTuple(Tuple t);
+    
+    string getName();
+    Scheme getScheme();
+    set<Tuple> getSet();
+    void setScheme(Scheme s);
+    
+    Relation select(string constant, int index, Relation relation);
+    Relation select(int index1, int index2, Relation relation);
+    Relation project(vector<int> positions, Relation relation);
+    Relation rename(vector<string> variables, Relation relation);
+    
+    Relation join(Relation relation1, Relation relation2);
+    void unite(Relation relation1);
+private:
+    string name; // Name of Scheme
+    Scheme scheme; // Scheme is one vector of attributes
+    set<Tuple> tupleSet; // These are Facts
 };
 
-#endif /* RELATION_H */
+#endif /* Relation_h */
