9e00cf709d4f16bf3e357fb8708546ed58a81cfd
elliking1 <71565785+elliking1@users.noreply.github.com>
2021-04-14 20:47:06 -0600

    Update DatalogProgram.h

--- Project 4/DatalogProgram.h
+++ Project 4/DatalogProgram.h
@@ -1,11 +1,3 @@
-//
-//  DatalogProgram.h
-//  Project2
-//
-//  Created by Cam on 2/6/19.
-//  Copyright © 2019 Cam Brown. All rights reserved.
-//
-
 #ifndef DatalogProgram_h
 #define DatalogProgram_h
 
